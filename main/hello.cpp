e501ea9f87afe4160b1cd3c29b3cb893f114b11e
GolamariBadrinath <golamaribadrinath@gmail.com>
2025-03-13 13:27:21 +0530

    Update hello.cpp message for Jenkins and Docker

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins and Docker!\n";
+  cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "I made changes to this file   \n";
   return 0;
 }
